2411e85760f1122649409281e07b024df262a2bc
henny0 <hubert.borowski02@gmail.com>
2021-10-05 12:53:05 +0000

    Input i css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
+const element = 3;
+const Normal = (
+    <h1>Hello World!</h1>
+)
+class Hello extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "World"
+        }
+    }
 
-const element = <h1>Hello World</h1>
-ReactDOM.render (element, document.getElementById("root"));
+    render() {
+        return <> 
+        <h1>Hello {this.state.name} </h1>
+        <input type="text" onChange={(event) => this.change(event)} />
+        </>
+    }
+    change(event){
+        console.log(event.target.value);
+        this.setState({name: event.target.value});
+    }
+}
+ const Box = ( <>
+    {Normal}
+    <hr/>
+    <Hello/>
+    </>
+ )
+ReactDOM.render(Box, document.getElementById('root'));
